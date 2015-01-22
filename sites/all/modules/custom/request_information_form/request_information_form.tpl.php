7831e2b83b37eb09638af5ae7ba5ff7cd446dc19
Tim Johnson <tim@mjdinteractive.com>
2015-01-21 17:13:31 -0800

    adding HTML5 input validation to email field in contact form

--- sites/all/modules/custom/request_information_form/request_information_form.tpl.php
+++ sites/all/modules/custom/request_information_form/request_information_form.tpl.php
@@ -3,9 +3,9 @@
   <input type=hidden name="oid" value="00Do0000000JSTB">
   <input type=hidden name="retURL" value="<?php print $base_url; ?>/thank-you">
 
-  <div class="form-group"><label for="first_name">First Name</label><input id="request-info-first-name" class="form-control" id="first_name" maxlength="40" name="first_name" size="20" type="text" placeholder="First Name" /></div>
+  <div class="form-group"><label for="first_name">First Name</label><input id="request-info-first-name" class="form-control" id="first_name" maxlength="40" name="first_name" size="20" required type="text" placeholder="First Name" /></div>
 
-  <div class="form-group"><label for="last_name">Last Name</label><input id="request-info-last-name" class="form-control" id="last_name" maxlength="80" name="last_name" size="20" type="text" placeholder="Last Name" /></div>
+  <div class="form-group"><label for="last_name">Last Name</label><input id="request-info-last-name" class="form-control" id="last_name" maxlength="80" name="last_name" size="20" required type="text" placeholder="Last Name" /></div>
 
   <div class="form-group"><label for="email">Email</label><input id="request-info-email" class="form-control" id="email" maxlength="80" name="email" size="20" type="email" required placeholder="Email" /></div>
 
