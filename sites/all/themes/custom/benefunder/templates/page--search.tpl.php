9d08593f191479289d126882ed127408ff63d5e1
Tim Johnson <tim@mjdinteractive.com>
2015-01-23 16:39:07 -0800

    themeing search

--- sites/all/themes/custom/benefunder/templates/page--search.tpl.php
+++ sites/all/themes/custom/benefunder/templates/page--search.tpl.php
@@ -91,21 +91,22 @@
 
   <?php if (isset($page['content']['system_main']['search_results']['search_results'])): ?>
 
-    <div class="col-xs-12 col-md-7 main-content">
+    <div class="col-xs-12 <?php if (!empty($page['content']['system_main']['search_results']['search_pager']['#markup'])) { print 'col-md-7'; } ?> main-content">
       <h2 class="search-results-label">Search Results</h2>
       <?php print render($page['content']['system_main']['search_results']['search_results']); ?>
     </div>
 
-    <div class="col-xs-12 col-md-4 col-md-offset-1 sidebar-second">
-      <?php print render($page['content']['system_main']['search_results']['search_pager']); ?>
-    </div>
+    <?php if (isset($page['content']['system_main']['search_results']['search_pager'])): ?>
+      <div class="col-xs-12 col-md-4 col-md-offset-1 sidebar-second">
+        <?php print render($page['content']['system_main']['search_results']['search_pager']); ?>
+      </div>
+    <?php endif; ?>
 
   <?php else: ?>
     <div class="col-xs-12 main-content">
       <h2 class="search-results-label">Your search yielded no results</h2>
       <?php print render($page['content']['system_main']['search_results']); ?>
     </div>
-
   <?php endif; ?>
 
 </div>
