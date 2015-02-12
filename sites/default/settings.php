12d36a5af2932bd5b57054c3838ec52693ba19ea
Tim Johnson <tim@mjdinteractive.com>
2015-02-12 11:16:57 -0800

    redirecting other domains to benefunder.org

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -10,3 +10,36 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
     exit();
   }
 }
+
+//Redirect benefunder.info
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
+  $_SERVER['PANTHEON_ENVIRONMENT'] === 'live') {
+  if ($_SERVER['HTTP_HOST'] == 'www.benefunder.info' ||
+      $_SERVER['HTTP_HOST'] == 'benefunder.info') {
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: http://benefunder.org'. $_SERVER['REQUEST_URI']);
+    exit();
+  }
+}
+
+//Redirect benefunder.net
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
+  $_SERVER['PANTHEON_ENVIRONMENT'] === 'live') {
+  if ($_SERVER['HTTP_HOST'] == 'www.benefunder.net' ||
+      $_SERVER['HTTP_HOST'] == 'benefunder.net') {
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: http://benefunder.org'. $_SERVER['REQUEST_URI']);
+    exit();
+  }
+}
+
+//Redirect benefunder.com
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
+  $_SERVER['PANTHEON_ENVIRONMENT'] === 'live') {
+  if ($_SERVER['HTTP_HOST'] == 'www.benefunder.com' ||
+      $_SERVER['HTTP_HOST'] == 'benefunder.com') {
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: http://benefunder.org'. $_SERVER['REQUEST_URI']);
+    exit();
+  }
+}
