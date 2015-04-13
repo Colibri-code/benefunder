13a9ea6e70dac4fef01de0b9f0da57b803b27a22
Tim Johnson <tim@mjdinteractive.com>
2015-04-13 16:50:24 -0700

    modified gulpfile

--- sites/all/themes/custom/benefunder/gulpfile.js
+++ sites/all/themes/custom/benefunder/gulpfile.js
@@ -1,7 +1,7 @@
 // Include gulp & gulp plugins
 var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
-  less = require('gulp-less'),
+  less = require('gulp-less-sourcemap'),
   stylish = require('jshint-stylish'),
   autoprefixer = require('gulp-autoprefixer');
 
@@ -15,21 +15,32 @@ gulp.task('lint', function() {
 // Compile LESS files
 gulp.task('less', function() {
   return gulp.src('less/style.less')
-    .pipe(less())
-    .pipe(autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'IE 9'],
-      cascade: true
+    .pipe(less({
+      sourceMap: {
+        sourceMapRootpath: '../less'
+      }
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('./css'));
+});
+
+// Autoprefix CSS
+gulp.task('autoprefix', function() {
+  return gulp.src('./css/style.css')
+      .pipe(autoprefixer({
+        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'IE 9'],
+        cascade: true
+      }))
+    .pipe(gulp.dest('./css'));
 });
 
 // Watch files for changes
 gulp.task('watch', function() {
   gulp.watch('js/main.js', ['lint']);
   gulp.watch('less/*.less', ['less']);
+  gulp.watch('css/style.css', ['autoprefix']);
 });
 
 // Default task
-gulp.task('default', ['lint', 'less', 'watch']);
+gulp.task('default', ['lint', 'less', 'autoprefix', 'watch']);
 
 
