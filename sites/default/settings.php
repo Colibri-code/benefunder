7005bc7ced6268f9a785c4c49fc820473c13d6de
Karl Scheirer <karl.scheirer@gmail.com>
2016-11-08 17:52:23 -0800

    use stock 404_fast_paths

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -76,6 +76,6 @@ if (defined('PANTHEON_ENVIRONMENT')) {
  * Add leading hash signs if you would like to disable this functionality.
  */
 $conf['404_fast_paths_exclude'] = '/\/(?:styles)|(?:system\/files)\//';
-$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp|wp\-login|wp\-admin)$/i';
+$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 drupal_fast_404();
