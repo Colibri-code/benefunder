9fca03c9c5030ef723d95af08bda131be99328ba
Karl Scheirer <karl.scheirer@gmail.com>
2017-01-16 14:28:07 -0800

    PT-45: add create proposal module

---
+++ sites/all/modules/custom/create_proposal/entity-print--node--cause.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * PDF template for printing.
+ *
+ * Available variables are:
+ *  - $entity - The entity itself.
+ *  - $entity_array - The renderable array of this entity.
+ *  - $entity_print_css - An array of stylesheets to be rendered.
+ */
+?>
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>PDF</title>
+</head>
+<body>
+<div class="page">
+  Entity Smurf! <br/>
+  <?php print get_class($entity); ?><br/>
+  <?php print $entity->title ?>
+</div>
+</body>
+</html>
