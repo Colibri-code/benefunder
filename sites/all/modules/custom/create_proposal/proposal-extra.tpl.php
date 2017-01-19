fe5da76539069eadf68611103d72db43a9729d00
Karl Scheirer <karl.scheirer@gmail.com>
2017-01-19 13:34:27 -0800

    PT-47: add addendum tpl file

---
+++ sites/all/modules/custom/create_proposal/proposal-extra.tpl.php
@@ -0,0 +1,28 @@
+<html>
+
+<body>
+
+<h1>Benefunder Wire and ACH Information</h1>
+
+<p>
+  Please notify Benefunder in writing at support@benefunder.com with any questions or problems.
+</p>
+
+<p><strong>ABA Number for Wires: </strong><?php print $aba_number ?></p>
+
+<p><strong>ACH: </strong><?php print $ach_number ?></p>
+
+<p><strong>Bank Name: </strong><?php print $bank_name ?></p>
+
+<p><strong>Account Number: </strong><?php print $account_number ?></p>
+
+<p><strong>Account Holder: </strong><?php print $account_holder ?></p>
+
+<p><strong>Bank Address: </strong><?php print $bank_street ?></p>
+
+<p><strong>City/State: </strong><?php print $bank_city_state ?></p>
+
+<p><strong>SWITFT Code: </strong><?php print $swift_code ?></p>
+
+</body>
+</html>
