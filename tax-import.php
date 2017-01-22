5461506ac45d4c404ab0149f8a959161c6889a7d
Karl Scheirer <karl.scheirer@gmail.com>
2017-01-21 17:11:23 -0800

    tax import, use like for title since some have leading space :(

--- tax-import.php
+++ tax-import.php
@@ -10,15 +10,19 @@ while ($row = fgetcsv($f)) {
     $taxonomy_array = taxonomy_get_term_by_name($row[0], 'aging_wellness_disease');
     $current_term = $row[0];
     $current_tid = array_pop($taxonomy_array)->tid;
+    if (empty($current_tid)) {
+      echo 'Could not find term id for: ' . $row[0] . PHP_EOL;
+      continue;
+    }
   }
 
-  $cause_nid = db_query('SELECT n.nid FROM {node} n WHERE n.title = :title AND n.type = :type', array(
-    ':title'=> $row[1],
+  $cause_nid = db_query('SELECT n.nid FROM {node} n WHERE n.title LIKE :title AND n.type = :type', array(
+    ':title'=> '%' . db_like($row[1]) . '%',
     ':type'=> 'cause')
   )->fetchField();
 
   if (empty($cause_nid)) {
-    echo 'Could not find node ID for :' . $row[1] . PHP_EOL;
+    echo 'Could not find node ID for: ' . $row[1] . PHP_EOL;
   }
   else {
     $cause_wrapper = entity_metadata_wrapper('node', $cause_nid);
