efea8341d0f0c4b909a72ae05dec9c39bccb255e
Karl Scheirer <karl.scheirer@gmail.com>
2017-02-28 09:08:12 -0800

    BS-30: add reason and textarea to request information form

--- sites/all/modules/custom/request_information_form/request_information_form.tpl.php
+++ sites/all/modules/custom/request_information_form/request_information_form.tpl.php
@@ -5,11 +5,27 @@
   <input type=hidden id="recordType" name="recordType" value="012o0000000clFV" />
   <input type=hidden id="lead_source" name="lead_source" value="Web inquiry" />
 
+  <div style="margin-bottom: 6px">I am interested in Benefunder as a <select id="00No000000EvSzN" name="00No000000EvSzN" class="form-control">
+      <option value="">-None-</option>
+      <option value="Advisor">Advisor</option>
+      <option value="Donor">Donor</option>
+      <option value="Financial Institution">Financial Institution</option>
+      <option value="Investor">Investor</option>
+      <option value="Employment">Employment</option>
+      <option value="Media Outlet">Media Outlet</option>
+      <option value="Researcher / Innovator">Researcher / Innovator</option>
+      <option value="University">University</option>
+      <option value="Other">Other</option>
+    </select>
+  </div>
+
   <div class="form-group"><label for="first_name">First Name</label><input id="request-info-first-name" class="form-control" id="first_name" maxlength="40" name="first_name" size="20" required type="text" placeholder="First Name" /></div>
 
   <div class="form-group"><label for="last_name">Last Name</label><input id="request-info-last-name" class="form-control" id="last_name" maxlength="80" name="last_name" size="20" required type="text" placeholder="Last Name" /></div>
 
   <div class="form-group"><label for="email">Email</label><input id="request-info-email" class="form-control" id="email" maxlength="80" name="email" size="20" type="email" required placeholder="Email" /></div>
+
+  <div class="form-group"><label for="Notes">Notes</label><textarea id="00No000000EvSzS" name="Notes" type="text" wrap="soft" placeholder="Notes" class="form-control"></textarea></div>
 
   <div class="form-group"><label for="phone">Phone</label><input id="request-info-phone" class="form-control" id="phone" maxlength="40" name="phone" size="20" type="text" placeholder="Phone (Optional)" /><button type="submit" class="btn btn-default">Send</button></div>
 
