068e1b480c249ac9d37660d25b790db327754a17
Karl Scheirer <karl.scheirer@gmail.com>
2017-05-04 11:58:07 -0700

    more info on the needs assessment page of pdf

--- sites/all/modules/custom/create_proposal/proposal-needs-assessment.tpl.php
+++ sites/all/modules/custom/create_proposal/proposal-needs-assessment.tpl.php
@@ -1,8 +1,6 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <script src="https://use.typekit.net/jxh1lif.js"></script>
-  <script>try{Typekit.load({ async: true });}catch(e){}</script>
   <style type="text/css">
     body {
       font-family: "Raleway", sans-serif;
@@ -30,16 +28,13 @@
       text-align: right;
       font-weight: bold;
     }
-    td:first-child:after {
-      content: ":";
-    }
   </style>
 </head>
 
 <body>
 
 <div class="header">
-<h2>Giving Profile</h2>
+<h1>Giving Profile</h1>
 <p>Benefunder provides you with impact in your giving: a unique opportunity to find, fund, and follow
 research that supports your charitable interests in an efficient, cost effective manner.</p>
 </div>
@@ -56,12 +51,44 @@ research that supports your charitable interests in an efficient, cost effective
     <td><?php print $citystate; ?></td>
   </tr>
   <tr>
+    <td>Relationship</td>
+    <td><?php print $relationship; ?></td>
+  </tr>
+  <tr>
     <td>University Alumni Associations</td>
     <td><?php print $alumni_associations; ?></td>
   </tr>
+  <tr>
+    <td>What problems are you most interested in solving?</td>
+    <td><?php print $what_problems_are_you_most; ?></td>
+  </tr>
+  <tr>
+    <td>Philanthropic Interests</td>
+    <td><?php print $philanthropic_interests; ?></td>
+  </tr>
+  <tr>
+    <td>What is your primary motivation behind your philanthropy</td>
+    <td><?php print $what_is_your_primary_motiv; ?></td>
+  </tr>
+  <tr>
+    <td>In addition to your gift, would you like to volunteer or get involved?</td>
+    <td><?php print $in_addition_to_providing_f; ?></td>
+  </tr>
+  <tr>
+    <td>Research Stages</td>
+    <td><?php print $research_stages; ?></td>
+  </tr>
+  <tr>
+    <td>Accomplishments</td>
+    <td><?php print $accomplishments; ?></td>
+  </tr>
+  <tr>
+    <td>Engagements</td>
+    <td><?php print $engagements; ?></td>
+  </tr>
 </table>
 
-<h3>Categories of Research Interests</h3>
+<!--<h3>Categories of Research Interests</h3>-->
 <h3>Current Charitable Giving</h3>
 <h3>Anticipated Giving Plan</h3>
 
