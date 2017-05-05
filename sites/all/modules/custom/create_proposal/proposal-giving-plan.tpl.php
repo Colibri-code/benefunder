0360fc5978d61726de110a114f9947e7185800bb
Karl Scheirer <karl.scheirer@gmail.com>
2017-05-05 14:48:40 -0700

    use css3/html graph for allocations, additional sections added

--- sites/all/modules/custom/create_proposal/proposal-giving-plan.tpl.php
+++ sites/all/modules/custom/create_proposal/proposal-giving-plan.tpl.php
@@ -25,6 +25,88 @@
       text-align: right;
       font-weight: bold;
     }
+    .pieContainer {
+      height: 200px;
+    }
+    .pieBackground {
+      background-color: grey;
+      position: absolute;
+      width: 200px;
+      height: 200px;
+      border-radius: 100px;
+    }
+    .pie {
+      position: absolute;
+      width: 200px;
+      height: 200px;
+      border-radius: 100px;
+      clip: rect(0px, 100px, 200px, 0px);
+    }
+    .hold {
+      position: absolute;
+      width: 200px;
+      height: 200px;
+      border-radius: 100px;
+      clip: rect(0px, 200px, 200px, 100px);
+    }
+    #pieSlice1 .pie {
+      background-color: red;
+      transform:rotate(50deg);
+      -webkit-transform:rotate(50deg);
+      -moz-transform:rotate(50deg);
+      -o-transform:rotate(50deg);
+    }
+    #pieSlice2 {
+      transform:rotate(50deg);
+      -webkit-transform:rotate(50deg);
+      -moz-transform:rotate(50deg);
+      -o-transform:rotate(50deg);
+    }
+    #pieSlice2 .pie {
+      background-color: yellow;
+      transform:rotate(50deg);
+      -webkit-transform:rotate(50deg);
+      -moz-transform:rotate(50deg);
+      -o-transform:rotate(50deg);
+    }
+    #pieSlice3 {
+      transform:rotate(100deg);
+      -webkit-transform:rotate(100deg);
+      -moz-transform:rotate(100deg);
+      -o-transform:rotate(100deg);
+    }
+    #pieSlice3 .pie {
+      background-color: blue;
+      transform:rotate(100deg);
+      -webkit-transform:rotate(100deg);
+      -moz-transform:rotate(100deg);
+      -o-transform:rotate(100deg);
+    }
+    #pieSlice4 {
+      transform:rotate(200deg);
+      -webkit-transform:rotate(200deg);
+      -moz-transform:rotate(200deg);
+      -o-transform:rotate(200deg);
+    }
+    #pieSlice4 .pie {
+      background-color: green;
+      transform:rotate(160deg);
+      -webkit-transform:rotate(160deg);
+      -moz-transform:rotate(160deg);
+      -o-transform:rotate(160deg);
+    }
+    .life {
+      color: red;
+    }
+    .tech {
+      color: yellow;
+    }
+    .env {
+      color: blue;
+    }
+    .arts {
+      color: green;
+    }
   </style>
 </head>
 
@@ -32,115 +114,58 @@
 
 <h1>Giving Plan</h1>
 
+<h3>Distribution Management</h3>
 Initial Contribution: $1,000,000.00<br/>
 Planned Annual Contribution: $250,000.00<br/>
+Targeted Growth Rate: 4%<br/>
 Annual Distribution Percentage: 15%<br/>
 
-<br/>
-<table>
-  <tr>
-    <th>2017</th>
-    <th>Q1</th>
-    <th>Q2</th>
-    <th>Q3</th>
-    <th>Q4</th>
-    <th>TOTAL</th>
-  </tr>
-  <tr>
-    <td>Darren Lipomi (50%)</td>
-    <td>1.875%</td>
-    <td>1.875%</td>
-    <td>1.875%</td>
-    <td>1.875%</td>
-    <td>7.5%</td>
-  </tr>
-  <tr>
-    <td>John Pintar (30%)</td>
-    <td>1.125%</td>
-    <td>1.125%</td>
-    <td>1.125%</td>
-    <td>1.125%</td>
-    <td>4.5%</td>
-  </tr>
-  <tr>
-    <td>Cause 3 (20%)</td>
-    <td>0.75%</td>
-    <td>0.75%</td>
-    <td>0.75%</td>
-    <td>0.75%</td>
-    <td>3%</td>
-  </tr>
-  <tr>
-    <td>Total</td>
-    <td>3.75%</td>
-    <td>3.75%</td>
-    <td>3.75%</td>
-    <td>3.75%</td>
-    <td>15%</td>
-  </tr>
-  <tr>
-    <td>Benefunder Fee (10%)</td>
-    <td colspan="4"></td>
-    <td>1.5%</td>
-  </tr>
-  <tr>
-    <td>Total Distribution</td>
-    <td colspan="4"></td>
-    <td>16.5%</td>
-  </tr>
-</table>
-<br/>
-<table>
-  <tr>
-    <th>2018</th>
-    <th>Q1</th>
-    <th>Q2</th>
-    <th>Q3</th>
-    <th>Q4</th>
-    <th>TOTAL</th>
-  </tr>
-  <tr>
-    <td>Darren Lipomi (50%)</td>
-    <td>1.875%</td>
-    <td>1.875%</td>
-    <td>1.875%</td>
-    <td>1.875%</td>
-    <td>7.5%</td>
-  </tr>
-  <tr>
-    <td>John Pintar (30%)</td>
-    <td>1.125%</td>
-    <td>1.125%</td>
-    <td>1.125%</td>
-    <td>1.125%</td>
-    <td>4.5%</td>
-  </tr>
-  <tr>
-    <td>Cause 3 (20%)</td>
-    <td>0.75%</td>
-    <td>0.75%</td>
-    <td>0.75%</td>
-    <td>0.75%</td>
-    <td>3%</td>
-  </tr>
-  <tr>
-    <td>Total</td>
-    <td>3.75%</td>
-    <td>3.75%</td>
-    <td>3.75%</td>
-    <td>3.75%</td>
-    <td>15%</td>
-  </tr>
-  <tr>
-    <td>Benefunder Fee (10%)</td>
-    <td colspan="4"></td>
-    <td>1.5%</td>
-  </tr>
-  <tr>
-    <td>Total Distribution</td>
-    <td colspan="4"></td>
-    <td>16.5%</td>
-  </tr>
-</table>
+<h3>Investment Strategy</h3>
+Funds will be invested in a balanced portfolio of stocks, bonds, and mutual funds. Please see investment policy statement.
+
+<h3>Allocation</h3>
+
+<div class="pieContainer">
+  <div class="pieBackground"></div>
+  <div id="pieSlice1" class="hold"><div class="pie"></div></div>
+  <div id="pieSlice2" class="hold"><div class="pie"></div></div>
+  <div id="pieSlice3" class="hold"><div class="pie"></div></div>
+  <div id="pieSlice4" class="hold"><div class="pie"></div></div>
+</div>
+
+<div class="legend">
+  <ul>
+    <li class="life">Life Sciences (13.89%)</li>
+    <ul class="life">
+      <li>Genomics/Congenital</li>
+      <li>Neurological/Cognitive</li>
+    </ul>
+    <li class="tech">Technology (13.89%)</li>
+    <ul class="tech">
+      <li>Computational Sciences/Mathematics</li>
+      <li>Informational Sciences/Internet</li>
+      <li>Robotics</li>
+    </ul>
+    <li class="env">Environment (27.78%)</li>
+    <ul class="env">
+      <li>Energy</li>
+    </ul>
+    <li class="arts">Humanities (44.44%)</li>
+    <ul class="arts">
+      <li>Archaeology</li>
+      <li>Economics</li>
+      <li>Law/Ethics</li>
+    </ul>
+  </ul>
+</div>
+
+<h3>Fees</h3>
+
+Total Annual Fund Overhead as a percent of fund balance: 1.93%<br/>
+Benefunder Service Fee: 10%<br/>
+Service fee taken from the fund, not the gift, at the time of distribution<br/>
+Administrative Fee - 50 Basis Points on Net Asset Value<br/>
+Financial institution fees may apply<br/>
+
 </body>
 </html>
