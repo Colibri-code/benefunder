e3695d73fd0824b36d625f93059c80d317cd3c96
Karl Scheirer <karl.scheirer@gmail.com>
2017-05-23 13:47:02 -0700

    proposal cover page, wip

--- sites/all/modules/custom/create_proposal/proposal-cover.tpl.php
+++ sites/all/modules/custom/create_proposal/proposal-cover.tpl.php
@@ -3,46 +3,81 @@
   <meta charset="UTF-8">
   <script src="https://use.typekit.net/jxh1lif.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
   <style type="text/css">
-    html {
-      height: 100%;
-    }
+    /* Define page size. Requires print-area adjustment! */
     body {
-      background-image: url(<?php print $background; ?>);
-      background-size: 100% 100%;
-      font-family: "Raleway", sans-serif;
-      height: 100%;
-      width: 100%;
-      border: 15px solid black;
-      margin: 0;
+      margin:     0;
+      padding:    0;
+      width:      21cm;
+      height:     29.7cm;
     }
+
+    /* Printable area */
+    #print-area {
+      position:   relative;
+      top:        1cm;
+      left:       1cm;
+      width:      19cm;
+      height:     27.6cm;
+
+      font-family: "Raleway", sans-serif;
+
+      background-image: url(<?php print $background; ?>);
+      background-size: 17cm, 20cm, cover;
+      background-clip: border-box;
+
+      border: 1px solid black;
+    }
+
+    #header {
+      height:     3cm;
+
+      background: #ccc;
+    }
+    #footer {
+      position:   absolute;
+      bottom:     0;
+      width:      100%;
+      height:     3cm;
+
+      background: #ccc;
+    }
+/*    html {*/
+/*      height: 11in;*/
+/*      width: 8.5in;*/
+/*      margin: 0;*/
+/*      padding: 0;*/
+/*    }*/
+/*    body {*/
+/*      margin: 0;*/
+/*      padding: 0;*/
+/*      width: 8.5in;*/
+/*      height: 11in;*/
+/*      font-family: "Raleway", sans-serif;*/
+/*      background-image: url(*/<?php //print $background; ?>/*);*/
+/*      background-size: 8in, 10in, cover;*/
+/*      background-clip: content-box;*/
+/*    }*/
     h1 {
       position: absolute;
       top: 200px;
       padding: 25px;
       color: black;
       text-transform: uppercase;
-      border-left: 15px solid #9F18A3;
-      background: rgba(0, 0, 0, 0.5);
-      margin-left: -15px;
+      background: rgba(102, 102, 102, 0.50);
     }
     h1 span {
       font-size: 125%;
     }
-    #right {
-      background: black;
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      right: 0;
-      width: 15px;
-    }
   </style>
+
 </head>
 <body>
 
-<div id="right"></div>
-<h1><?php print $name; ?><br/><span>Needs Assessment</span></h1>
+<div id="print-area">
+  <h1><?php print $name; ?><br/><span>Needs Assessment</span></h1>
+</div>
 
 </body>
 </html>
