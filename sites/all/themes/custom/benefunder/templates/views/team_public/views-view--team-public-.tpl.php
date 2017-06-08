31c14b086dfee15a39170a2df24512772632dc88
Igor Pinchuk <ipinchuk@devengineering.com>
2017-06-08 17:43:48 +0300

    Updated template

--- sites/all/themes/custom/benefunder/templates/views/team_public/views-view--team-public-.tpl.php
+++ sites/all/themes/custom/benefunder/templates/views/team_public/views-view--team-public-.tpl.php
@@ -29,16 +29,16 @@
 ?>
 
 <?php if (!empty($rows)): ?>
-<div class="bio-group">
-  <div class="bio-group__label">
-    <?php if ($title): ?>
-      <?php print $title; ?>
-    <?php endif; ?>
+  <div class="bio-group">
+    <div class="bio-group__label">
+      <?php if ($title): ?>
+        <?php print $title; ?>
+      <?php endif; ?>
+    </div>
+    <div class="bio-group__content">
+      <?php print $rows; ?>
+    </div>
   </div>
-  <div class="bio-group__content">
-    <?php print $rows; ?>
-  </div>
-</div>
 <?php endif; ?>
 
 <?php if ($attachment_after): ?>
