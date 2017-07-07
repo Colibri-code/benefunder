c24d4d80e70921a90e4ef2a2ee5768e7d63e4993
Karl Scheirer <karl.scheirer@gmail.com>
2017-07-07 12:27:05 -0700

    field upddates, text tweaks, split distribution percent and spend down period into 2 questions

--- sites/all/modules/features/bf_giving_proposal/total-allocations.js
+++ sites/all/modules/features/bf_giving_proposal/total-allocations.js
@@ -2,11 +2,27 @@
 Drupal.behaviors.totalAllocations = {
   attach: function (context, settings) {
     jQuery(document).mouseup(function () {
+
       var sum = 0;
-      jQuery('.sliderfield-display-values-field').each(function() {
-          sum += parseInt(jQuery(this).text().substring(1));
-      });
-      sum *= 1000;
+
+      // Allocations are dollar amounts in thousands, show total selected.
+      //jQuery('.sliderfield-display-values-field').each(function() {
+      //    sum += parseInt(jQuery(this).text().substring(1));
+      //});
+      //sum *= 1000;
+      //jQuery('input[name=total_allocations]').val('$' + addCommas(sum));
+
+      // Allocations are % instead of dollar amounts, show estimated total available.
+      var intent           = jQuery('#edit-field-what-is-your-long-term-int-und').val();
+
+      var annual_cont      = parseInt(jQuery('.sliderfield-field-adjust-field-planned-annual-contributio').val());
+      var initial_cont     = parseInt(jQuery('.sliderfield-field-adjust-field-planned-initial-contributi').val());
+
+      var distribution_pct = parseInt(jQuery('.sliderfield-field-adjust-field-distribution-percentage').val());
+      var spend_down       = parseInt(jQuery('.sliderfield-field-adjust-field-spend-down-period').val());
+
+
+      var sum = (initial_cont * distribution_pct / 100);
       jQuery('input[name=total_allocations]').val('$' + addCommas(sum));
     })
   }
