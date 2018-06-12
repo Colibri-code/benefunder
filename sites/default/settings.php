929aed77516d57928ec3c802e68bc65c4c955d6d
m3chas <joazrivera@gmail.com>
2018-06-12 00:24:33 -0600

    Updated gitignore, added local settings conditiontal on settings file, added workflow module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Include a local settings file if it exists. D7 only
+ */
+$local_settings = dirname(__FILE__) . '/settings.local.php';
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
+
 // Force HTTPS.
 if (isset($_SERVER['PANTHEON_ENVIRONMENT'])
     && php_sapi_name() != 'cli'
